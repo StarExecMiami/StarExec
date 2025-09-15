320a294ab2964e4acd131209f4bd5e42c1fceab7
Andres Caicedo <Andrezz1997@gmail.com>
2025-09-14 22:16:41 -0500

    Remove obsolete sample data scripts from the SQL directory. These scripts were previously used for loading sample data and inserting test data into the database, but are no longer needed.

--- sql/sample-data/StarSample.sql
+++
@@ -1,6 +0,0 @@
--- Author: Todd Elvers
--- Description: This file loads the sample data in this directory into the starexec database
-
-source Basic.sql;
-source Benchmarks.sql;
-source JobPairs.sql;
