4689936ac37876980be9b97f894d6f19dde7e603
Andres Caicedo <Andrezz1997@gmail.com>
2025-11-13 14:33:11 -0500

    test(AdminPictureUpload): Enhance admin picture upload authorization tests

--- src/test/java/org/starexec/test/junit/data/security/AdminPictureUploadTest.java
+++ src/test/java/org/starexec/test/junit/data/security/AdminPictureUploadTest.java
@@ -1,6 +1,8 @@
 package org.starexec.test.junit.data.security;
 
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.starexec.data.database.Users;
 import org.starexec.data.security.GeneralSecurity;
 
@@ -13,67 +15,85 @@ public class AdminPictureUploadTest {
 
     @Test
     public void testAdminCanUploadOwnPicture() {
-        // Test admin user (ID=1) uploading own picture
-        int adminUserId = 1;
-        int targetUserId = 1; // own picture
+        try (MockedStatic<GeneralSecurity> generalSecurityMock = Mockito.mockStatic(GeneralSecurity.class);
+             MockedStatic<Users> usersMock = Mockito.mockStatic(Users.class)) {
+            // Test admin user (ID=1) uploading own picture
+            int adminUserId = 1;
+            int targetUserId = 1; // own picture
 
-        boolean callerIsOwner = (targetUserId == adminUserId);
-        boolean callerIsAdmin = GeneralSecurity.hasAdminWritePrivileges(adminUserId);
-        boolean isPublicUser = Users.isPublicUser(adminUserId);
+            generalSecurityMock.when(() -> GeneralSecurity.hasAdminWritePrivileges(adminUserId)).thenReturn(true);
+            usersMock.when(() -> Users.isPublicUser(adminUserId)).thenReturn(false);
 
-        System.out.println("Admin upload own picture test:");
-        System.out.println("callerIsOwner: " + callerIsOwner);
-        System.out.println("callerIsAdmin: " + callerIsAdmin);
-        System.out.println("isPublicUser: " + isPublicUser);
+            boolean callerIsOwner = (targetUserId == adminUserId);
+            boolean callerIsAdmin = GeneralSecurity.hasAdminWritePrivileges(adminUserId);
+            boolean isPublicUser = Users.isPublicUser(adminUserId);
 
-        // Authorization logic from UploadPicture.java line 74
-        boolean shouldDeny = !(callerIsOwner || callerIsAdmin) || isPublicUser;
+            System.out.println("Admin upload own picture test:");
+            System.out.println("callerIsOwner: " + callerIsOwner);
+            System.out.println("callerIsAdmin: " + callerIsAdmin);
+            System.out.println("isPublicUser: " + isPublicUser);
 
-        System.out.println("Should deny: " + shouldDeny);
-        assertFalse("Admin should be able to upload own picture", shouldDeny);
+            // Authorization logic from UploadPicture.java line 74
+            boolean shouldDeny = !(callerIsOwner || callerIsAdmin) || isPublicUser;
+
+            System.out.println("Should deny: " + shouldDeny);
+            assertFalse("Admin should be able to upload own picture", shouldDeny);
+        }
     }
 
     @Test
     public void testAdminCanUploadOtherPicture() {
-        // Test admin user (ID=1) uploading another user's picture
-        int adminUserId = 1;
-        int targetUserId = 2; // another user's picture
+        try (MockedStatic<GeneralSecurity> generalSecurityMock = Mockito.mockStatic(GeneralSecurity.class);
+             MockedStatic<Users> usersMock = Mockito.mockStatic(Users.class)) {
+            // Test admin user (ID=1) uploading another user's picture
+            int adminUserId = 1;
+            int targetUserId = 2; // another user's picture
 
-        boolean callerIsOwner = (targetUserId == adminUserId);
-        boolean callerIsAdmin = GeneralSecurity.hasAdminWritePrivileges(adminUserId);
-        boolean isPublicUser = Users.isPublicUser(adminUserId);
+            generalSecurityMock.when(() -> GeneralSecurity.hasAdminWritePrivileges(adminUserId)).thenReturn(true);
+            usersMock.when(() -> Users.isPublicUser(adminUserId)).thenReturn(false);
 
-        System.out.println("Admin upload other picture test:");
-        System.out.println("callerIsOwner: " + callerIsOwner);
-        System.out.println("callerIsAdmin: " + callerIsAdmin);
-        System.out.println("isPublicUser: " + isPublicUser);
+            boolean callerIsOwner = (targetUserId == adminUserId);
+            boolean callerIsAdmin = GeneralSecurity.hasAdminWritePrivileges(adminUserId);
+            boolean isPublicUser = Users.isPublicUser(adminUserId);
 
-        // Authorization logic from UploadPicture.java line 74
-        boolean shouldDeny = !(callerIsOwner || callerIsAdmin) || isPublicUser;
+            System.out.println("Admin upload other picture test:");
+            System.out.println("callerIsOwner: " + callerIsOwner);
+            System.out.println("callerIsAdmin: " + callerIsAdmin);
+            System.out.println("isPublicUser: " + isPublicUser);
 
-        System.out.println("Should deny: " + shouldDeny);
-        assertFalse("Admin should be able to upload other user's picture", shouldDeny);
+            // Authorization logic from UploadPicture.java line 74
+            boolean shouldDeny = !(callerIsOwner || callerIsAdmin) || isPublicUser;
+
+            System.out.println("Should deny: " + shouldDeny);
+            assertFalse("Admin should be able to upload other user's picture", shouldDeny);
+        }
     }
 
     @Test
     public void testNonAdminCannotUploadOtherPicture() {
-        // Test non-admin user trying to upload another user's picture
-        int nonAdminUserId = 2; // assuming user 2 is not admin
-        int targetUserId = 3; // another user's picture
+        try (MockedStatic<GeneralSecurity> generalSecurityMock = Mockito.mockStatic(GeneralSecurity.class);
+             MockedStatic<Users> usersMock = Mockito.mockStatic(Users.class)) {
+            // Test non-admin user trying to upload another user's picture
+            int nonAdminUserId = 2; // assuming user 2 is not admin
+            int targetUserId = 3; // another user's picture
 
-        boolean callerIsOwner = (targetUserId == nonAdminUserId);
-        boolean callerIsAdmin = GeneralSecurity.hasAdminWritePrivileges(nonAdminUserId);
-        boolean isPublicUser = Users.isPublicUser(nonAdminUserId);
+            generalSecurityMock.when(() -> GeneralSecurity.hasAdminWritePrivileges(nonAdminUserId)).thenReturn(false);
+            usersMock.when(() -> Users.isPublicUser(nonAdminUserId)).thenReturn(false);
 
-        System.out.println("Non-admin upload other picture test:");
-        System.out.println("callerIsOwner: " + callerIsOwner);
-        System.out.println("callerIsAdmin: " + callerIsAdmin);
-        System.out.println("isPublicUser: " + isPublicUser);
+            boolean callerIsOwner = (targetUserId == nonAdminUserId);
+            boolean callerIsAdmin = GeneralSecurity.hasAdminWritePrivileges(nonAdminUserId);
+            boolean isPublicUser = Users.isPublicUser(nonAdminUserId);
 
-        // Authorization logic from UploadPicture.java line 74
-        boolean shouldDeny = !(callerIsOwner || callerIsAdmin) || isPublicUser;
+            System.out.println("Non-admin upload other picture test:");
+            System.out.println("callerIsOwner: " + callerIsOwner);
+            System.out.println("callerIsAdmin: " + callerIsAdmin);
+            System.out.println("isPublicUser: " + isPublicUser);
 
-        System.out.println("Should deny: " + shouldDeny);
-        assertTrue("Non-admin should NOT be able to upload other user's picture", shouldDeny);
+            // Authorization logic from UploadPicture.java line 74
+            boolean shouldDeny = !(callerIsOwner || callerIsAdmin) || isPublicUser;
+
+            System.out.println("Should deny: " + shouldDeny);
+            assertTrue("Non-admin should NOT be able to upload other user's picture", shouldDeny);
+        }
     }
 }
