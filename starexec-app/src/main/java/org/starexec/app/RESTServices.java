6c7b1abecadf90ee99b4316604a5c09c0d645691
Andres Caicedo <Andrezz1997@gmail.com>
2026-04-10 11:57:35 -0500

    fix(api): authorize processor lookup by job creation access

--- starexec-app/src/main/java/org/starexec/app/RESTServices.java
+++ starexec-app/src/main/java/org/starexec/app/RESTServices.java
@@ -119,6 +119,55 @@ public class RESTServices {
 	private static final int MAX_UPLOAD_SESSION_CREATE_BODY_BYTES = 64 * 1024;
 
 	@GET
+	@Path("/space/{sid}/processors")
+	@Produces("application/json")
+	public String getProcessorsBySpace(@PathParam("sid") int spaceId, @Context HttpServletRequest request) {
+		int userId = SessionUtil.getUserId(request);
+		ValidatorStatusCode status = JobSecurity.canUserCreateJobInSpace(userId, spaceId);
+		if (!status.isSuccess()) {
+			return gson.toJson(status);
+		}
+		try {
+			int communityId = Spaces.getCommunityOfSpace(spaceId);
+			if (communityId <= 0) {
+				return gson.toJson(new ValidatorStatusCode(false, "Space not found"));
+			}
+			List<Processor> post = Processors.getByCommunity(communityId,
+					org.starexec.data.to.enums.ProcessorType.POST);
+			List<Processor> pre = Processors.getByCommunity(communityId,
+					org.starexec.data.to.enums.ProcessorType.PRE);
+
+			List<Map<String, Object>> postProcessors = new ArrayList<>();
+			if (post != null) {
+				for (Processor processor : post) {
+					Map<String, Object> simple = new HashMap<>();
+					simple.put("id", processor.getId());
+					simple.put("name", processor.getName());
+					postProcessors.add(simple);
+				}
+			}
+
+			List<Map<String, Object>> preProcessors = new ArrayList<>();
+			if (pre != null) {
+				for (Processor processor : pre) {
+					Map<String, Object> simple = new HashMap<>();
+					simple.put("id", processor.getId());
+					simple.put("name", processor.getName());
+					preProcessors.add(simple);
+				}
+			}
+
+			Map<String, Object> response = new HashMap<>();
+			response.put("postProcessors", postProcessors);
+			response.put("preProcessors", preProcessors);
+			return gson.toJson(response);
+		} catch (Exception e) {
+			log.error("Error retrieving processors for space " + spaceId, e);
+			return gson.toJson(ERROR_DATABASE);
+		}
+	}
+
+	@GET
 	@Path("/queue/{qid}/getDesc")
 	@Produces("text/plain")
 	public static String getDescription(@PathParam("qid") int qid) {
